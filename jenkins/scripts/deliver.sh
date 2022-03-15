0d7317bec9e8f41d7dd0f479c40eef985f20ac82
ottavio-jeco <53174309+ottavio-jeco@users.noreply.github.com>
2022-03-15 15:38:56 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,3 +24,12 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
+set +x
+
+echo 'The following command runs docker build'
+set -x
+docker build --build-arg JAR_FILE=target/*.jar -t testmavenprj .
+
+
+
+
